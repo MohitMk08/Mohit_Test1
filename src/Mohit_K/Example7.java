4d3d27332917b58ebb5e8fae2d3dd02353c44014
Softcell@Mohit <mohitkuttarmare55@gmail.com>
2021-09-26 18:38:46 +0530

    Mohit_Practice_Examples

---
+++ src/Mohit_K/Example7.java
@@ -0,0 +1,21 @@
+package Mohit_K;
+
+public class Example7 {
+	String name;
+	static double price;
+	static  char qGrade = 'A';
+	
+	public static void main(String[] args) {
+		Example7 example7 = new Example7();
+		example7.price = 93.23;
+		example7.name = "Mohit";
+		example7.qGrade = 'B';
+		
+		Example7 example7_1 = new Example7();
+		example7_1.qGrade = 'C';
+		example7_1.qGrade = 'D';
+		
+		System.out.println(example7.name + "-" + example7.price + "-" + example7.qGrade);
+		
+	}
+}
