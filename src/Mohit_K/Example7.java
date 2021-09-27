7a0136fd1f695da6a7ec2f4a562f1daa5cdd03d9
Softcell@Mohit <mohitkuttarmare55@gmail.com>
2021-09-27 20:31:03 +0530

    Assignment6

--- src/Mohit_K/Example7.java
+++ src/Mohit_K/Example7.java
@@ -2,8 +2,8 @@ package Mohit_K;
 
 public class Example7 {
 	String name;
-	static double price;
-	static  char qGrade = 'A';
+	 double price;
+	  char qGrade = 'A';
 	
 	public static void main(String[] args) {
 		Example7 example7 = new Example7();
@@ -15,7 +15,7 @@ public class Example7 {
 		example7_1.qGrade = 'C';
 		example7_1.qGrade = 'D';
 		
-		System.out.println(example7.name + "-" + example7.price + "-" + example7.qGrade);
+		System.out.println(example7.name + "-" + example7.price + "-" + example7_1.qGrade);
 		
 	}
 }
